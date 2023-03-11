c5774596c17d7ae496e8e6c3b159ebc745c8938a
zhanjia <564508540@qq.com>
2023-03-11 11:47:00 +0800

    first

--- Master/storage.py
+++ Master/storage.py
@@ -17,3 +17,5 @@ class Storage:
         print(123,self.agents) #123 {'8bfce475806e469b8d64012ed1dfc1a7': ('8bfce475806e469b8d64012ed1dfc1a7', 'zhanjia-pc', ['200.200.200.88'])}
     # def reg_hb(self,id,hostname,ip):
     #     self.agents][id]=Agent(id,hostname,ip)
+    # def reg_hb(self,id,hostname,ip):
+    #     self.agents][id]=Agent(id,hostname,ip)
